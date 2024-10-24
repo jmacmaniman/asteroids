3d36232930cc255c8671ff4ea7c6fc39fe0a8524
jmacmaniman <26492003+jmacmaniman@users.noreply.github.com>
2024-10-23 17:07:25 -0700

    can draw a blank screen

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# this allows us to use code from the open-source pygame library
 import pygame
 from constants import *
 
@@ -6,6 +5,17 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
