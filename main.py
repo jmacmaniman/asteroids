360c509b02dbec685120f40db724346cc24e709a
jmacmaniman <26492003+jmacmaniman@users.noreply.github.com>
2024-10-25 16:04:56 -0700

    Player triangle is drawn on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -7,13 +8,21 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player_x = SCREEN_WIDTH / 2
+    player_y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(player_x, player_y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60))/1000
 
 
 
