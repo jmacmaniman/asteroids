39e3e7fb68853d64fb2e3f28a6c6becba8634b9c
jmacmaniman <26492003+jmacmaniman@users.noreply.github.com>
2024-10-26 16:54:03 -0700

    l/r rotation added

--- player.py
+++ player.py
@@ -18,3 +18,15 @@ class Player(CircleShape):
 
     def draw(self, screen):
         return pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), width=2)
+
+    def rotate(self, dt):
+        self.rotation = (PLAYER_TURN_SPEED * dt)
+        return self.rotation
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
